bf006d3af73e1aa18143546a1ec9fd13f645feff
Artem <139495869+yakimi27@users.noreply.github.com>
2026-04-02 13:26:54 +0300

    add constants class for supported image extensions

---
+++ OsuBackgroundReplacerMain/Logic/Constants.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace OsuBackgroundReplacerMain.Logic
+{
+    public static class Constants
+    {
+        public static readonly string[] SupportedImageExtensions = { ".jpg", ".jpeg", ".png" };
+
+        public static bool IsSupportedImage(string filePath)
+        {
+            string? extension = Path.GetExtension(filePath)?.ToLower();
+            return extension != null && SupportedImageExtensions.Contains(extension);
+        }
+    }
+}
